8365bd2658ef3282b1a54762b0a2dbf9a4cc796a
Liz Gi Oviedo Luna <95966086+egoluna@users.noreply.github.com>
2025-12-09 16:00:44 -0300

    Add files via upload

---
+++ estudiantes/rubioSardi/script.js
@@ -0,0 +1,20 @@
+let index = 0;
+
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+function showSlide(i) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[i].classList.add("active");
+}
+
+next.addEventListener("click", () => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+});
+
+prev.addEventListener("click", () => {
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index);
+});
